9142d2a1d6308e83d2ef8722e8a0b102897217ea
Augusto-brasil <augusto.brasiljck@gmail.com>
2026-06-04 10:34:12 -0300

    validacção de class para requisicação

--- pythonJobs.py
+++ pythonJobs.py
@@ -4,7 +4,7 @@ import pandas
 
 #1 - coeltando vagas de python
 
-url = 'https://www.timesjobs.com/job-search?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation='
+url = 'https://books.toscrape.com/catalogue/category/books/travel_2/index.html'
 
 print('enviando requisiçao')
 
@@ -12,3 +12,8 @@ response = requests.get(url)
 
 print(f'status code: {response.status_code}')
 
+soup = BeautifulSoup(response.text, 'lxml')
+jobs = soup.find_all('li', class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
+
+print(len(jobs))
+print(jobs[:3])
