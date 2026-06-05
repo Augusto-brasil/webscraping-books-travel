e5a9e7ad88e4aa60b473fc6c71d3d92a1a37b13e
Augusto-brasil <augusto.brasiljck@gmail.com>
2026-06-05 14:51:57 -0300

    projeto finalizado, coms os segunites passos: 1 - extração/raspagem de dados de livros sobre viage com seus respectivos preços. 2 - tratamento dos dados, como restirada de nomes e preços de cada book. 3- criação de um dataframe para melhro trabalhar com os dados. 4 - por fim a ciração de arquivo csv com os dadossolicitado limpos e visual.

---
+++ precosLivrosVieagem.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+#1 - coeltando vagas de python
+
+url = 'https://books.toscrape.com/catalogue/category/books/travel_2/index.html'
+
+print('enviando requisiçao')
+
+response = requests.get(url)
+
+print(f'status code: {response.status_code}')
+
+soup = BeautifulSoup(response.text, 'lxml')
+books = soup.find_all('li', class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
+
+book_name = []
+book_price = []
+
+for book in books:
+    n_tag = book.select_one('h3 a')
+    name = n_tag['title'] if n_tag else 'inexistente'
+    book_name.append(name)
+
+    price = book.find('p', class_='price_color')
+    p_book = price.get_text()[2:] if price else "zerado"
+    book_price.append(p_book)
+    
+df_livros = pd.DataFrame()
+df_livros['Names'] = book_name
+df_livros['Prices'] = book_price
+
+df_livros.to_csv('livrosdeviagens.csv', index=False, encoding='utf-8')
+print("Arquivo gerado com sucesso !!!")
+
+
+
+
+
