f9a5c9f26a1830795c8908bdd7e7e382ae9cd6c5
Augusto-brasil <augusto.brasiljck@gmail.com>
2026-06-05 14:29:14 -0300

    final do fluxo e criação correta das listas de nomes dos books e seus respectivos preços

--- pythonJobs.py
+++ pythonJobs.py
@@ -19,14 +19,16 @@ book_price = []
 
 for book in books:
     n_tag = book.select_one('h3 a')
-    name = n_tag['title'] if book else 'inexistente'
+    name = n_tag['title'] if n_tag else 'inexistente'
     book_name.append(name)
 
-    # price = book.find('p', class_='price_color')
-    # if price:
-    #     p_book = price.get_text()
-for x in book_name:
-    print(x)
+    price = book.find('p', class_='price_color')
+    p_book = price.get_text()[2:] if price else "zerado"
+    book_price.append(p_book)
+
+
+for x_nome, Y_preço in zip(book_name, book_price):
+    print(f'nome: {x_nome} - preco:{Y_preço}')
 
 
 
